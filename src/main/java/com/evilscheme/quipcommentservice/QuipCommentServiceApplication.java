9d232c7b0d58252e7f471c862e02d30fcdd59c06
Matheu DeWitt <msdewitt@bsu.edu>
2018-03-24 16:15:50 -0400

    Project init.

---
+++ src/main/java/com/evilscheme/quipcommentservice/QuipCommentServiceApplication.java
@@ -0,0 +1,12 @@
+package com.evilscheme.quipcommentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuipCommentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuipCommentServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/evilscheme/quipcommentservice/QuipCommentServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.evilscheme.quipcommentservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuipCommentServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
